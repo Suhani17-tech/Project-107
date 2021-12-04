f4bfb35f90586e179c55ab02ddde3dac2eee0075
Suhani17-tech <82748709+Suhani17-tech@users.noreply.github.com>
2021-12-04 20:59:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//https://teachablemachine.withgoogle.com/models/Bt8y3DReL/model.json
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier
+    ('https://teachablemachine.withgoogle.com/models/Bt8y3DReL/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results){
+console.log('gotResults');
+}
